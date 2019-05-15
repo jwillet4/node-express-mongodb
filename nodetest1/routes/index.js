6504d9261ce5f549a54f956d5adf0f2171891328
Logan Willett <lwillett@pgsioffice.com>
2019-05-15 16:55:41 -0400

    Not working correctly yet, but added database and new userlist page.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Get helloworld page
 router.get('/helloworld', function(req, res) {
   res.render('helloworld', { title: 'Hello, World!' });
 });
+
+//Get user list page
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('userlist', {
+      "userlist" : docs
+    })
+  })
+})
 
 module.exports = router;
